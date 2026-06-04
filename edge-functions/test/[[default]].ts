64496df672b2d28759a163899e524faa16211fcf
Nattsu39 <nattsu39@outlook.com>
2026-06-04 16:41:56 +0800

    修复导出

--- edge-functions/test/[[default]].ts
+++ edge-functions/test/[[default]].ts
@@ -1,7 +1,7 @@
 import { getStore } from "@edgeone/pages-blob";
 import { type EventContext } from "../v1/_common.js";
 
-export default async function onRequestGet(context: EventContext): Promise<Response> {
+export async function onRequestGet(context: EventContext): Promise<Response> {
     const store = getStore("test-store");
     const data = await store.get("data/skill/id.json", { type: "json" });
     const { request, params } = context;
